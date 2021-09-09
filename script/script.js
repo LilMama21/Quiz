ad97e672e19e360ba3e4b88f42161522dc54e0a9
LilMama21 <78875152+LilMama21@users.noreply.github.com>
2021-09-09 18:52:33 +0300

    Add files via upload
    Перебор массива с вопросами

--- script/script.js
+++ script/script.js
@@ -4,38 +4,141 @@ document.addEventListener("DOMContentLoaded", function () {
   const closeModal = document.querySelector("#closeModal");
   const questionTitle = document.querySelector("#question");
   const formAnswers = document.querySelector("#formAnswers");
-  const burgerName = "Черный";
-  const burgerBlackImageUrl = `./image/burgerBlack.png`;
+  const prevButton = document.querySelector("#prev");
+  const nextButton = document.querySelector("#next");
+  const questions = [
+    {
+      question: "Какого цвета бургер?",
+      answers: [
+        {
+          title: "Стандарт",
+          url: "./image/burger.png",
+        },
+        {
+          title: "Черный",
+          url: "./image/burgerBlack.png",
+        },
+      ],
+      type: "radio",
+    },
+    {
+      question: "Из какого мяса котлета?",
+      answers: [
+        {
+          title: "Курица",
+          url: "./image/chickenMeat.png",
+        },
+        {
+          title: "Говядина",
+          url: "./image/beefMeat.png",
+        },
+        {
+          title: "Свинина",
+          url: "./image/porkMeat.png",
+        },
+      ],
+      type: "radio",
+    },
+    {
+      question: "Дополнительные ингредиенты?",
+      answers: [
+        {
+          title: "Помидор",
+          url: "./image/tomato.png",
+        },
+        {
+          title: "Огурец",
+          url: "./image/cucumber.png",
+        },
+        {
+          title: "Салат",
+          url: "./image/salad.png",
+        },
+        {
+          title: "Лук",
+          url: "./image/onion.png",
+        },
+      ],
+      type: "checkbox",
+    },
+    {
+      question: "Добавить соус?",
+      answers: [
+        {
+          title: "Чесночный",
+          url: "./image/sauce1.png",
+        },
+        {
+          title: "Томатный",
+          url: "./image/sauce2.png",
+        },
+        {
+          title: "Горчичный",
+          url: "./image/sauce3.png",
+        },
+      ],
+      type: "radio",
+    },
+  ];
 
   btnOpenModal.addEventListener("click", () => {
     modalBlock.classList.add("d-block");
     playTest();
-  })
+  });
 
   closeModal.addEventListener("click", () => {
     modalBlock.classList.remove("d-block");
-    })
+  });
 
   const playTest = () => {
-    const renderQuestion = () => {
-      questionTitle.textContent = "Какого цвета бургер Вы хотите?";
+    let numbersQuestion = 0;
 
-      formAnswers.innerHTML = `
-      <div class="answers-item d-flex flex-column">
-          <input type="radio" id="answerItem1" name="answer" class="d-none">
-          <label for="answerItem1" class="d-flex flex-column justify-content-between">
-          <img class="answerImg" src="./image/burger.png" alt="burger">
-          <span>Стандарт</span>
-          </label>
-      </div>
-      <div class="answers-item d-flex justify-content-center">
-          <input type="radio" id="answerItem2" name="answer" class="d-none">
-          <label for="answerItem2" class="d-flex flex-column justify-content-between">
-          <img class="answerImg" src=${burgerBlackImageUrl} alt="burger">
-          <span>${burgerName}</span>
-          </label>
-      </div>`
-    }
-    renderQuestion();
-  }
-})
+    const renderAnswers = (index) => {
+      questions[index].answers.forEach((ans) => {
+        const answerItem = document.createElement("div");
+
+        answerItem.classList.add("answers-item", "d-flex", "flex-column");
+
+        answerItem.innerHTML = `
+				<input type="${questions[index].type}" id="${ans.title}" name="answer" class="d-none">
+				<label for="${ans.title}" class="d-flex flex-column justify-content-between">
+					<img class="answerImg" src="${ans.url}" alt="burger">
+					<span>${ans.title}</span>
+				</label>
+				`;
+        formAnswers.appendChild(answerItem);
+      });
+    };
+
+    const renderQuestions = (indexQuestion) => {
+      formAnswers.innerHTML = "";
+
+      if (numbersQuestion == 0) {
+        prevButton.classList.add("d-none");
+      } else {
+        prevButton.classList.remove("d-none");
+      }
+
+      if (numbersQuestion == questions.length - 1) {
+        nextButton.classList.add("d-none");
+      } else {
+        nextButton.classList.remove("d-none");
+      }
+
+      questionTitle.textContent = `${questions[indexQuestion].question}`;
+
+      renderAnswers(indexQuestion);
+    };
+    renderQuestions(numbersQuestion);
+
+    nextButton.onclick = () => {
+      numbersQuestion++;
+      renderQuestions(numbersQuestion);
+    };
+
+    prevButton.onclick = () => {
+      numbersQuestion--;
+      renderQuestions(numbersQuestion);
+    };
+  };
+});
